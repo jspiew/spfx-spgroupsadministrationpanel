4fd8628455ab436de761c5d8aeacd299f3093f6a
jspiewak <jkspiewak@gmail.com>
2018-10-07 17:19:42 +0200

    further work on details view

--- src/webparts/sharePointGroupsAdminPanel/components/GroupsDetailsView.tsx
+++ src/webparts/sharePointGroupsAdminPanel/components/GroupsDetailsView.tsx
@@ -11,8 +11,8 @@ import { SPHttpClient } from '@microsoft/sp-http';
 import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
 import { AbbrToggle } from "./small/abbrToggle"
 import { Draft } from '../../../utils/draft';
-import { Dialog } from '@microsoft/sp-dialog/lib/index';
 import { css } from '@uifabric/utilities/lib';
+import { Dialog, DialogType, DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
 
 export interface IGroupsDetailsViewState {
     
@@ -28,19 +28,21 @@ export interface IGroupsDetailsViewProps {
 export default class GroupsDetailsView extends React.Component<IGroupsDetailsViewProps, IGroupsDetailsViewState> {
     public render(): React.ReactElement<IGroupsDetailsViewProps> {
         return (
-            <div className={css("ms-Grid", styles.groupsDetailsView)}>
-                <div className="ms-Grid-row">
-                    {this.props.groups.map((g) => {
-                        return (
-                            <div className="ms-Grid-col ms-sm4">
-                                <div>
-                                    <h4 className={styles.groupTitle}>{g.Title}</h4>  
-                                    <i className={css("ms-Icon", "ms-Icon--PeopleAdd", styles.peopleAddIcon)} aria-hidden="true"></i>
+            <div>
+                <div className={css("ms-Grid", styles.groupsDetailsView)}>
+                    <div className="ms-Grid-row">
+                        {this.props.groups.map((g) => {
+                            return (
+                                <div className="ms-Grid-col ms-sm4">
+                                    <div>
+                                        <h4 className={styles.groupTitle}>{g.Title}</h4>  
+                                        <i className={css("ms-Icon", "ms-Icon--PeopleAdd", styles.peopleAddIcon)} aria-hidden="true"></i>
+                                    </div>
+                                    {g.Users.map(u => <SpUserPersona user={u}/>)}
                                 </div>
-                                {g.Users.map(u => <SpUserPersona user={u}/>)}
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         )
